a342c0bd1c162dac73e31e0a5b280739b8de206a
Joshua Gabriel Soriano <joshuagabriel011@gmail.com>
2024-04-12 19:22:22 +0800

    updates to card focusing(finally working)

--- js/recipecardfocus.js
+++ js/recipecardfocus.js
@@ -1,27 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const card = document.querySelector(".cards");
+    //const card = document.querySelector(".cards"); old card var
+    const cards = document.querySelectorAll(".cards");
     const overlay = document.querySelector(".overlay");
+    let currentlyfocusedcard = null;
+
 
     // focus card
-    function focusCard() {
+    function focusCard(card) {
+        if (currentlyfocusedcard !== null) {
+            currentlyfocusedcard.classList.remove("focused");
+        }
+        
         card.classList.add("focused");
         overlay.style.display = "block";
+        currentlyfocusedcard = card;
     }
 
     //unfocus card
     function unfocusCard() {
-        card.classList.remove("focused");
-        overlay.style.display = "none";
+        if (currentlyfocusedcard == null) {
+            console.log("Error walay focused card")
+        }
+        else {
+            currentlyfocusedcard.classList.remove("focused");
+            overlay.style.display = "none";
+        }
     }
 
-    //card click check
+    //card click check old
 
-    card.addEventListener("click",function() {
-        if (card.classList.contains("focused")) {
-            unfocusCard(card);
+    // card.addEventListener("click",function() {
+    //     if (card.classList.contains("focused")) {
+    //         unfocusCard(card);
+    //         } else {
+    //         focusCard(card);
+    //         }
+    // });
+
+    //card click check new
+    cards.forEach(card => {
+        card.addEventListener("click", function() {
+            if (card.classList.contains("focused")) {
+                unfocusCard();
             } else {
-            focusCard(card);
+                focusCard(card);
             }
+        });
     });
     
     //overlay click check
